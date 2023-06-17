a16ed66316e1fb7d7abd993cdd1862aac4b6ca5e
daniel99estrada <pildoestrada@gmail.com>
2023-06-17 17:44:01 -0400

    added DynamoDb Integration

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template
+import boto3
+
+AWS_ACCESS_KEY = "AKIA5UD2B5HDXF5XQUMR"
+AWS_SECRET_KEY = "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"
+AWS_REGION_NAME = "us-east-1"
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+@app.route('/', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    newsletter = request.form.get('newsletter', False)
+
+    # Create an AWS session
+    session = boto3.Session(
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+        region_name=AWS_REGION_NAME
+    )
+
+    # Save the response values in AWS DynamoDB table
+    dynamodb = session.resource('dynamodb')
+    table = dynamodb.Table('Messages')
+    table.put_item(
+        Item={
+            'name': name,
+            'email': email,
+            'message': message,
+            'newsletter': newsletter
+        }
+    )
+    return 'Message submitted successfully!'
 
 @app.route('/')
 def index():
